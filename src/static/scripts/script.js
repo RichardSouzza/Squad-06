22169d642c7b8b46f8496d36d19d142ba7704932
Richard Souza <souzza.richard25@gmail.com>
2023-10-11 17:26:53 -0300

    Add table of DAMs to `index.html`

---
+++ src/static/scripts/script.js
@@ -0,0 +1,24 @@
+function addRedBorders() {
+    const DAMs = document.getElementsByClassName('dam-row');
+    for (let dam of DAMs) {
+        const damRect = dam.getBoundingClientRect();
+        const leftBorder = document.createElement('div');
+        leftBorder.className = 'red-border';
+        leftBorder.style.height = `calc(${damRect.height}px - 0.5rem)`
+        leftBorder.style.left = `calc(${damRect.left}px + 0.5rem)`
+        leftBorder.style.top = `calc(${damRect.top}px + 0.25rem)`
+        dam.appendChild(leftBorder);
+        const rightBorder = document.createElement('div');
+        rightBorder.className = 'red-border';
+        rightBorder.style.height = `calc(${damRect.height}px - 0.5rem)`;
+        rightBorder.style.left = `calc(${damRect.right}px - 0.75rem)`;
+        rightBorder.style.top = `calc(${damRect.y}px + 0.25rem)`;
+        dam.appendChild(rightBorder);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+        addRedBorders();
+    }, 500);
+});
