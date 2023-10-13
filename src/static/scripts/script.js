d906f5623ea8c4f6e603f3582a11c197a4c1af6b
Richard Souza <souzza.richard25@gmail.com>
2023-10-12 21:31:26 -0300

    Add DAM print screen

--- src/static/scripts/script.js
+++ src/static/scripts/script.js
@@ -1,3 +1,7 @@
+const whatsappAlert = document.getElementById('whatsapp-alert');
+const emailAlert = document.getElementById('email-alert');
+
+
 function addRedBorders() {
     const body = document.getElementsByTagName('body')[0];
     const DAMs = document.getElementsByClassName('dam-row');
@@ -18,8 +22,79 @@ function addRedBorders() {
     }
 }
 
+
 document.addEventListener('DOMContentLoaded', function() {
     setTimeout(function() {
         addRedBorders();
     }, 500);
 });
+
+
+function closeAlerts() {
+    closeWhatsAppAlert();
+    closeEmailAlert();
+    document.removeEventListener('click', clickHandler);
+}
+
+
+function clickHandler(event) {
+    if (!whatsappAlert.contains(event.target)) {
+        closeAlerts()
+    }
+}
+
+
+function setClickHandlerEventListener() {
+    setTimeout(() => {
+        document.addEventListener('click', clickHandler);
+    }, 0);
+}
+
+
+function showWhatsAppAlert() {
+    closeAlerts();
+    whatsappAlert.style.display = 'block';
+    setClickHandlerEventListener();
+}
+
+
+function closeWhatsAppAlert() {
+    whatsappAlert.style.display = 'none';
+}
+
+
+function sendByWhatsApp() {
+    closeWhatsAppAlert();
+}
+
+
+function showEmailAlert() {
+    closeAlerts();
+    emailAlert.style.display = 'block';
+    setClickHandlerEventListener();
+}
+
+
+function closeEmailAlert() {
+    emailAlert.style.display = 'none';
+}
+
+
+function sendByEmail() {
+    closeEmailAlert();
+}
+
+
+function downloadDAM() {
+    const damUrl = 'static/data/DAM.pdf';
+    var a = document.createElement('a');
+    a.href = damUrl;
+    a.download = 'DAM.pdf';
+    a.click();
+}
+
+
+function printDAM() {
+    const damUrl = 'static/data/DAM.pdf';
+    window.location.href = damUrl;
+}
