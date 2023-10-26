aac31ede5bd45f017deacc154191ef03cf21503c
Richard Souza <souzza.richard25@gmail.com>
2023-10-26 20:37:28 -0300

    Change style

--- src/client/src/pages/Contribuinte.jsx
+++ src/client/src/pages/Contribuinte.jsx
@@ -1,9 +1,9 @@
 import { useEffect } from 'react';
-import ClientHeader from '../components/ClientHeader';
-import Footer from '../components/Footer';
+import ClientHeader from '../components/ClientHeader/ClientHeader';
+import Footer from '../components/Footer/Footer';
 import Code from '../assets/images/code-01.svg'
-import addRedBorders from '../assets/scripts/script';
-import '../assets/styles/style.css'
+import addRedBorders from '../assets/scripts/addRedBorders';
+import '../assets/styles/Contribuinte.css';
 
 export default function Contribuinte() {
   document.title = 'Portal do Contribuinte';
@@ -34,27 +34,27 @@ export default function Contribuinte() {
                 <td>IPTU - IPTU GRAND(100.00%)</td>
                 <td>Setembro de 2023</td>
                 <td>25/09/2023</td>
-                <td>1.000,00</td>
-                <td className="text-blue cursor-pointer" onClick="window.location.href='client/dam'">Gerar</td>
-                <td className="text-blue cursor-pointer" onClick="window.location.href='client/dam'">Exibir</td>
+                <td className="align-right">1.000,00</td>
+                <td className="table-anchor" onClick="window.location.href='client/dam'">Gerar</td>
+                <td className="table-anchor" onClick="window.location.href='client/dam'">Exibir</td>
               </tr>
               <tr className="space"></tr>
               <tr className="dam-row">
                 <td>ALUGUEL 2 - ALUGUEL(1.50%)</td>
                 <td>Setembro de 2023</td>
                 <td>15/09/2023</td>
-                <td>17,00</td>
-                <td className="text-blue cursor-pointer" onClick="window.location.href='client/dam'">Gerar</td>
-                <td className="text-blue cursor-pointer"></td>
+                <td className="align-right">17,00</td>
+                <td className="table-anchor" onClick="window.location.href='client/dam'">Gerar</td>
+                <td className="table-anchor"></td>
               </tr>
               <tr className="space"></tr>
               <tr className="dam-row">
                 <td>LIX - COLETA DE LIXO(1.00%)</td>
                 <td>Setembro de 2023</td>
                 <td>15/09/2023</td>
-                <td>2,00</td>
-                <td className="text-blue cursor-pointer" onClick="window.location.href='client/dam'">Gerar</td>
-                <td className="text-blue cursor-pointer"></td>
+                <td className="align-right">2,00</td>
+                <td className="table-anchor" onClick="window.location.href='client/dam'">Gerar</td>
+                <td className="table-anchor"></td>
               </tr>
             </tbody>
           </table>
