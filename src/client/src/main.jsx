44983246e40e0f47f1bf0d2259d9185464679e6c
Richard Souza <souzza.richard25@gmail.com>
2023-10-26 23:53:55 -0300

    Fix route

--- src/client/src/main.jsx
+++ src/client/src/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Contribuinte from './pages/Contribuinte';
 import Login from './pages/Login';
 import './assets/styles/index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <React.StrictMode>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/contribuinte" element={<Contribuinte />} />
+        <Route path="/client" element={<Contribuinte />} />
       </Routes>
     </React.StrictMode>
   </BrowserRouter>
