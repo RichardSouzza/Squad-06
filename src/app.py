bafe3f38e4fc1c5b650cfa139c62b9af75ae04cb
Richard <souzza.richard25@gmail.com>
2023-10-27 05:12:32 -0300

    Revert "Merge `react` branch"

--- src/app.py
+++ src/app.py
@@ -1,22 +1,34 @@
 from flask import Flask, render_template
 
 
-app = Flask(__name__,
-            template_folder="client/dist",
-            static_folder="client/dist",
-            static_url_path="")
+app = Flask(__name__)
 
 
 @app.route("/")
 def login():
-    return render_template("index.html")
+    return render_template("login.html")
+
+
+@app.route("/verification-2FT")
+def verification2FT():
+    return render_template("verificacao-2FT.html")
+
+
+@app.route("/verify-cellphone")
+def verify_cellphone():
+    return render_template("verificacao-celular.html")
+
+
+@app.route("/verify-email")
+def verify_email():
+    return render_template("verificacao-email.html")
 
 
 @app.route("/client")
 def client():
-    return render_template("index.html")
+    return render_template("contribuinte.html")
 
 
 @app.route("/client/dam")
 def dam():
-    return render_template("index.html")
+    return render_template("dam.html")
--- src/client/src/assets/scripts/addMask.js
+++
@@ -1,9 +0,0 @@
-export default function addMask(input) {
-  let x = input.value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,3})(\d{0,3})(\d{0,4})(\d{0,2})/);
-  input.value = !x[2] ? x[1] : x[1] + '.' + x[2] + (x[3] ? '.' : '') + x[3] + (x[4] ? '/' : x[4]) + x[4] + (x[5] ? '-' + x[5] : '');
-
-  if (input.value.length < 15) {
-      x = input.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,3})(\d{0,2})/);
-      input.value = !x[2] ? x[1] : x[1] + '.' + x[2] + (x[3] ? '.' : '') + x[3] + (x[4] ? '-' + x[4] : '');
-  }
-}
