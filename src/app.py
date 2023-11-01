47ac9317901c9511910bfe6a7c88c7bd284e9c4b
Richard Souza <souzza.richard25@gmail.com>
2023-11-01 14:30:14 -0300

    Revert #5

--- src/app.py
+++ src/app.py
@@ -1,19 +1,22 @@
 from flask import Flask, render_template
 
 
-app = Flask(__name__)
+app = Flask(__name__,
+            template_folder="client/dist",
+            static_folder="client/dist",
+            static_url_path="")
 
 
 @app.route("/")
 def login():
-    return render_template("login.html")
+    return render_template("index.html")
 
 
 @app.route("/client")
 def client():
-    return render_template("contribuinte.html")
+    return render_template("index.html")
 
 
 @app.route("/client/dam")
 def dam():
-    return render_template("dam.html")
+    return render_template("index.html")
---
+++ src/client/src/pages/Login.jsx
@@ -0,0 +1,35 @@
+import { useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+import addMask from '../assets/scripts/addMask';
+import '../assets/styles/login.css';
+
+export default function Login() {
+  document.title = 'Login';
+  const navigate = useNavigate();
+  useEffect(() => {
+    const input = document.getElementById('identification');
+    const form = document.getElementById('form');
+    input.pattern = '^\\d{2}\\.?\\d{3}\\.?\\d{3}\\/?\\d{4}-?\\d{2}|\\d{3}\\.?\\d{3}\\.?\\d{3}-?\\d{2}$';
+    input.addEventListener('keyup', () => {addMask(input)});
+    form.addEventListener('submit', () => {navigate('/client')});
+  });
+
+  return (
+    <div className="body">
+      <Header />
+      <main>
+        <div className="floatCard">
+          <p>Entrar com CPF ou CNPJ:</p>
+          <form id="form">
+            <input type="text" id="identification" name="identification" inputMode="numeric" title="12 números para CPF ou 14 números para CNPJ" required></input>
+            <input type="submit" value="ENTRAR"></input>
+          </form>
+          <p>Não consegue acessar? <a className="anchor">Clique aqui</a>.</p>
+        </div>
+      </main>
+      <Footer contactVisibility="hide"/>
+    </div>
+  );
+}
