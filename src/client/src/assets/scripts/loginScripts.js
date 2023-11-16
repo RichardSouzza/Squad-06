962a4f7c912aef2cebc5a8c90819970203a9c9a9
Richard Souza <souzza.richard25@gmail.com>
2023-11-16 16:15:38 -0300

    Implement ID validation

--- src/client/src/assets/scripts/loginScripts.js
+++ src/client/src/assets/scripts/loginScripts.js
@@ -1,4 +1,4 @@
-export default function addMask(input) {
+export function addMask(input) {
     let x = input.value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,3})(\d{0,3})(\d{0,4})(\d{0,2})/);
     input.value = !x[2] ? x[1] : x[1] + '.' + x[2] + (x[3] ? '.' : '') + x[3] + (x[4] ? '/' : x[4]) + x[4] + (x[5] ? '-' + x[5] : '');
 
@@ -6,4 +6,10 @@ export default function addMask(input) {
         x = input.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,3})(\d{0,2})/);
         input.value = !x[2] ? x[1] : x[1] + '.' + x[2] + (x[3] ? '.' : '') + x[3] + (x[4] ? '-' + x[4] : '');
     }
+}
+
+export function validateID(id) {
+    const invalidCPF =  '000.000.000-00';
+    const invalidCNPJ =  '00.000.000/0000-00';
+    return ![invalidCPF, invalidCNPJ].includes(id);
 }
--- src/client/src/pages/Login.jsx
+++ src/client/src/pages/Login.jsx
@@ -1,8 +1,10 @@
 import { useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import { error } from '../assets/scripts/toasts';
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
-import addMask from '../assets/scripts/addMask';
+import { addMask, validateID } from '../assets/scripts/loginScripts';
 import '../assets/styles/login.css';
 
 export default function Login() {
@@ -13,7 +15,18 @@ export default function Login() {
     const form = document.getElementById('form');
     input.pattern = '^\\d{2}\\.?\\d{3}\\.?\\d{3}\\/?\\d{4}-?\\d{2}|\\d{3}\\.?\\d{3}\\.?\\d{3}-?\\d{2}$';
     input.addEventListener('keyup', () => addMask(input));
-    form.addEventListener('submit', () => navigate('/verification'));
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+      if (validateID(input.value)) {
+        navigate('/verification');
+      } else {
+        if (input.value.length === 14) {
+          error('CPF inválido');
+        } else {
+          error('CNPJ inválido');
+        }
+      }
+    });
   });
 
   return (
@@ -23,12 +36,13 @@ export default function Login() {
         <div className="floatCard">
           <p>Entrar com CPF ou CNPJ:</p>
           <form id="form">
-            <input type="text" id="identification" name="identification" inputMode="numeric" title="12 números para CPF ou 14 números para CNPJ" required></input>
+            <input type="text" id="identification" name="identification" inputMode="numeric" title="11 números para CPF ou 14 números para CNPJ" required></input>
             <input type="submit" value="ENTRAR"></input>
           </form>
           <p>Não consegue acessar? <a className="anchor">Clique aqui</a>.</p>
         </div>
       </main>
+      <ToastContainer />
       <Footer contactVisibility="hide"/>
     </div>
   );
