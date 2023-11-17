5f94704467e0c188768f36bfd3120f30a433b88e
Richard Souza <souzza.richard25@gmail.com>
2023-11-16 22:58:00 -0300

    Implement countdown

--- src/client/src/pages/PINVerification.jsx
+++ src/client/src/pages/PINVerification.jsx
@@ -1,38 +1,64 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import { notify, error, limitError } from '../assets/scripts/toasts';
 import { assignInputFunctions, generatePIN, verifyPIN } from '../assets/scripts/verificationScripts';
+import Countdown from "react-countdown";
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 import Undo from '../assets/images/undo.svg';
 import 'react-toastify/dist/ReactToastify.css';
 import '../assets/styles/login.css';
 
+let countdownTime;
+
+function renderer({ minutes, seconds }) {
+  return (<span className="span-red bold">{`${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`}</span>);
+}
+
 export default function PINVerification() {
   document.title = 'Verificação de PIN';
   const navigate = useNavigate();
-  let defaultPIN = generatePIN();
-  let toastNotifyText = `Seu PIN é: ${defaultPIN}`;
-  const toastErrorText = 'PIN inválido';
-  const toastLimitErrorText = 'Suas tentativas acabaram';
+  const [resetKey, setResetKey] = useState(0);
+  countdownTime = Date.now() + 60000;
 
-  function regeneratePIN() {
-    toast.dismiss();
-    defaultPIN = generatePIN();
-    toastNotifyText = `Seu novo PIN é: ${defaultPIN}`;
-    notify(toastNotifyText);
+  function resetCountdown() {
+    setResetKey((prevKey) => prevKey + 1);
   }
-  
+
+  function setResendPINButtonEnablingState(state) {
+    const resendPINButton = document.getElementById('resendPINButton');
+    resendPINButton.disabled = !state;
+  }
+
+  function countdownOnComplete() {
+    setResendPINButtonEnablingState(true);
+  }
+
   useEffect(() => {
+    let defaultPIN = generatePIN();
+    let toastNotifyText = `Seu PIN é: ${defaultPIN}`;
+    const toastErrorText = 'PIN inválido';
+    const toastLimitErrorText = 'Suas tentativas acabaram';
+
+    function regeneratePIN() {
+      setResendPINButtonEnablingState(false);
+      countdownTime = Date.now() + 60000;
+      resetCountdown();
+      toast.dismiss();
+      defaultPIN = generatePIN();
+      toastNotifyText = `Seu novo PIN é: ${defaultPIN}`;
+      notify(toastNotifyText);
+    }
+
     notify(toastNotifyText);
     assignInputFunctions();
     const floatCard = document.getElementsByClassName('floatCard')[0];
     const form = document.getElementById('form');
     const attempts = document.getElementById('attempts');
-    const regeneratePINButton = document.getElementById('regeneratePINButton');
-    regeneratePINButton.addEventListener('click', regeneratePIN);
-    
+    const resendPINButton = document.getElementById('resendPINButton');
+    resendPINButton.addEventListener('click', regeneratePIN);
+
     function verifyPINError() {
       error(toastErrorText);
       attempts.innerText -= 1;
@@ -45,11 +71,7 @@ export default function PINVerification() {
       for (let input of floatCard.getElementsByTagName('input')) {
         input.disabled = true;
       }
-      regeneratePINButton.removeEventListener('click', regeneratePIN);
-      regeneratePINButton.addEventListener('click', () => {
-        toast.dismiss();
-        limitError(toastLimitErrorText);
-      });
+      setResendPINButtonEnablingState(false);
     }
 
     form.addEventListener('submit', (event) => {
@@ -74,7 +96,7 @@ export default function PINVerification() {
         inputs[0].focus();
       }
     });
-  });
+  }, [navigate]);
 
   return (
     <div className="body">
@@ -93,10 +115,10 @@ export default function PINVerification() {
           </form>
           <div id="info">
             <p className="text-xs">Você possui <span className="span-red bold" id="attempts">5</span> tentativas restantes.</p>
-            <p className="text-xs">Aguarde <span className="span-red bold" id="pin-delay">00:59</span> para pedir um novo código.</p>
+            <p className="text-xs">Aguarde <Countdown date={countdownTime} key={resetKey} renderer={renderer} onComplete={countdownOnComplete} /> para pedir um novo código.</p>
             <p className="text-xs">Ainda não recebeu o código? Verifique a caixa de spam do seu e-mail ou escolha uma das opções a seguir:</p>
           </div>
-          <button className="icon-button anchor" id="regeneratePINButton"><img src={Undo} alt="Reenviar" />REENVIAR CÓDIGO</button>
+          <button className="icon-button anchor" id="resendPINButton" disabled><img src={Undo} alt="Reenviar" />REENVIAR CÓDIGO</button>
           <button className="icon-button anchor">RECEBER DE OUTRAS FORMAS</button>
         </div>
       </main>
